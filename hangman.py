e1ce2cb57add8d5a9b370b0ac7e94968987b9079
molikpunj <moulickpunjdu@gmail.com>
2025-08-05 01:24:24 +0530

    Added a list and made it so that the game chooses a random word from the list

--- hangman.py
+++ hangman.py
@@ -1,1 +1,14 @@
 '''SIMPLE HANGMAN GAME!!!'''
+
+import random
+
+attempts = 15
+words = ["feline", "tiger", "country", "prosperous", "terrific", "anonymous", "explode", "surface", "supervise", "conscious"]
+
+def maingame():
+    chosen_word = random.choice(words)
+    display = ["_" for _ in chosen_word]
+    print(display)
+
+print("Welcome To Hangman!")
+maingame()
