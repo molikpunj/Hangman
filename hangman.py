d76c68f5b1eecd249d793ebde28d0140ed9c5f83
molikpunj <moulickpunjdu@gmail.com>
2025-08-10 02:00:25 +0530

    Added the guessing and changing array part.

--- hangman.py
+++ hangman.py
@@ -2,13 +2,24 @@
 
 import random
 
-attempts = 15
 words = ["feline", "tiger", "country", "prosperous", "terrific", "anonymous", "explode", "surface", "supervise", "conscious"]
 
 def maingame():
+    attempts = 15
     chosen_word = random.choice(words)
     display = ["_" for _ in chosen_word]
-    print(display)
+    while attempts > 0 and "_" in display:
+        for _ in display:
+            print(_, end = "")
+        guess = input(" Guess one letter: ")
+        attempts -= 1
+        print(f"The number of attempts left are: {attempts}")
+        if guess.lower() in chosen_word:
+            realguess = guess.lower()
+            for i, letter in enumerate(chosen_word):
+                if letter == guess:
+                    display[i] = realguess
+    print("You win!")
 
 print("Welcome To Hangman!")
 maingame()
